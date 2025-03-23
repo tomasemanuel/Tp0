6ead16bc90f3683f09aaf50ff5bc7980f8859c02
tomas <temanuel@fi.uba.ar>
2025-03-22 23:37:11 -0300

    test ej4

--- server/common/server.py
+++ server/common/server.py
@@ -1,13 +1,26 @@
 import socket
 import logging
+import signal
+import sys
 
 
 class Server:
     def __init__(self, port, listen_backlog):
-        # Initialize server socket
+
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+
+        self._running = True
+        signal.signal(signal.SIGTERM, self._graceful_shutdown)
+
+    def _graceful_shutdown(self, signum, frame):
+        logging.info("action: shutdown_signal_received")
+        self._running = False
+        self._server_socket.close()
+        logging.info("action: server_socket_close")
+        logging.info("action: exit | result: success")
+        sys.exit(0)
 
     def run(self):
         """
@@ -20,9 +33,14 @@ class Server:
 
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
-        while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+        while self._running:
+            try:
+                client_sock = self.__accept_new_connection()
+                if client_sock:
+                    self.__handle_client_connection(client_sock)
+            except OSError:
+                # Puede ser que el socket fue cerrado por SIGTERM
+                break
 
     def __handle_client_connection(self, client_sock):
         """
@@ -35,11 +53,13 @@ class Server:
             # TODO: Modify the receive to avoid short-reads
             msg = client_sock.recv(1024).rstrip().decode('utf-8')
             addr = client_sock.getpeername()
-            logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
+            logging.info(
+                f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
             # TODO: Modify the send to avoid short-writes
             client_sock.send("{}\n".format(msg).encode('utf-8'))
         except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
+            logging.error(
+                "action: receive_message | result: fail | error: {e}")
         finally:
             client_sock.close()
 
@@ -54,5 +74,6 @@ class Server:
         # Connection arrived
         logging.info('action: accept_connections | result: in_progress')
         c, addr = self._server_socket.accept()
-        logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
+        logging.info(
+            f'action: accept_connections | result: success | ip: {addr[0]}')
         return c
