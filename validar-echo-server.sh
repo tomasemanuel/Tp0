83d3d6155e31a21d3a768dc71bf7606e37e780c2
tomas <temanuel@fi.uba.ar>
2025-03-23 17:26:23 -0300

    Validar compose con testing_net

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-
 PORT=12345
-MESSAGE= "Hello world!"
+MESSAGE="Hello world!"
 TIMEOUT=5
 SERVER_CONTAINER="server"
+NETWORK="testing_net"
 
-RESPONSE=$(docker run --rm --network testing_net alpine:latest sh -c "echo $MESSAGE | nC -W $TIMEOUT $SERVER_CONTAINER $PORT")
+RESPONSE=$(docker run --rm --network "$NETWORK" alpine:latest sh -c "
+  apk add --no-cache netcat-openbsd > /dev/null &&
+  echo \"$MESSAGE\" | nc -w $TIMEOUT $SERVER_CONTAINER $PORT
+")
 
+# Mostrar respuesta obtenida para debug
+echo "Response from container: '$RESPONSE'"
+
+# Validación
 if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
