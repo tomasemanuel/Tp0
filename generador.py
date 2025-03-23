7ba143d0a424cc9a593c32e86b2d6f38f9026bce
tomas <temanuel@fi.uba.ar>
2025-03-23 17:59:49 -0300

    add .py file

---
+++ generador.py
@@ -0,0 +1,62 @@
+import sys
+import yaml
+
+
+import yaml
+
+
+def create_clients(file_path, clients):
+    # Estructura base del compose
+    data = {
+        'version': '3',
+        'services': {},
+        'networks': {
+            'testing_net': {
+                'ipam': {
+                    'driver': 'default',
+                    'config': [
+                        {'subnet': '172.25.125.0/24'}
+                    ]
+                }
+            }
+        }
+    }
+
+    # Agregar servidor
+    data['services']['server'] = {
+        'container_name': 'server',
+        'image': 'server:latest',
+        'entrypoint': 'python3 /main.py',
+        'environment': [
+            'PYTHONUNBUFFERED=1',
+            'LOGGING_LEVEL=DEBUG'
+        ],
+        'networks': ['testing_net'],
+        'volumes': ['./server/config.ini:/config.ini']
+    }
+
+    # Agregar clientes
+    for i in range(1, clients + 1):
+        data['services'][f'client{i}'] = {
+            'container_name': f'client{i}',
+            'image': 'client:latest',
+            'entrypoint': '/client',
+            'environment': [
+                f'CLI_ID={i}',
+                'CLI_LOG_LEVEL=DEBUG'
+            ],
+            'networks': ['testing_net'],
+            'depends_on': ['server'],
+            'volumes': ['./client/config.yaml:/config.yaml']
+        }
+
+    # Escribir archivo compose sobrescribiendo lo que había
+    with open(file_path, 'w') as docker_compose:
+        yaml.dump(data, docker_compose, default_flow_style=False)
+
+
+if __name__ == '__main__':
+    file = sys.argv[1]
+    clients = int(sys.argv[2])
+
+    create_clients(file, clients)
--- generar-compose.sh
+++ generar-compose.sh
@@ -1,53 +1,20 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "Uso: $0 <archivo_salida> <cantidad_clientes>"
+    echo "Error: Must enter 2 arguments"
     exit 1
 fi
 
-OUTPUT_FILE=$1
-NUM_CLIENTS=$2
+FILE_NAME=$1
+CLIENTS=$2
 
-cat > "$OUTPUT_FILE" <<EOL
-version: "3.8"
+# Verifico que el numero sea correcto (solo enteros)
+if ! [[ "$CLIENTS" =~ ^[0-9]+$ ]]; then
+    echo "Error: Client number must be an integer"
+    exit 1
+fi
 
-services:
-  server:
-    container_name: server
-    image: server:latest
-    entrypoint: python3 /main.py
-    environment:
-      - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
-    networks:
-      - testing_net
-EOL
+# Invoco un subscript de python para modificar el archivo de docker-compose.yaml
+python3 generador.py "$FILE_NAME" "$CLIENTS"
 
-for i in $(seq 1 "$NUM_CLIENTS"); do
-    cat >> "$OUTPUT_FILE" <<EOL
-
-  client$i:
-    container_name: client$i
-    image: client:latest
-    entrypoint: /client
-    environment:
-      - CLI_ID=$i
-      - CLI_LOG_LEVEL=DEBUG
-    networks:
-      - testing_net
-    depends_on:
-      - server
-EOL
-done
-
-cat >> "$OUTPUT_FILE" <<EOL
-
-networks:
-  testing_net:
-    ipam:
-      driver: default
-      config:
-        - subnet: 172.25.125.0/24
-EOL
-
-echo "Archivo $OUTPUT_FILE generado correctamente con $NUM_CLIENTS clientes."
+echo "Docker compose updated with $CLIENTS clients"
