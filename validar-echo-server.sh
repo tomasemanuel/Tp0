0d65899f74cba99c2ec4162c3e8225a7eac8cc74
tomas <temanuel@fi.uba.ar>
2025-03-23 16:13:35 -0300

    change to alpine

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-docker build -f Dockerfile.netcat -t netcat-test .
 
-RESPONSE=$(docker run --rm --network tp0_testing_net netcat-test)
+PORT=12345
+MESSAGE= "Hello world!"
+TIMEOUT=5
+SERVER_CONTAINER="server"
 
-echo "Response from container: '$RESPONSE'"
+RESPONSE=$(docker run --rm --network tpo_testing_net alpine:latest sh -c "echo $MESSAGE | nC -W $TIMEOUT $SERVER_CONTAINER $PORT")
 
-if [ "$RESPONSE" = "hello world" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
