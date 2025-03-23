84939be9fa7a3aa16c8fec5f7e44fe5575b68f22
tomas <temanuel@fi.uba.ar>
2025-03-23 17:45:44 -0300

    PUSH WITHOUT TIMEOUT

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -7,8 +7,7 @@ SERVER_CONTAINER="server"
 NETWORK="tp0_testing_net"
 
 RESPONSE=$(docker run --rm --network "$NETWORK" alpine:latest sh -c "
-  apk add --no-cache netcat-openbsd > /dev/null &&
-  echo \"$MESSAGE\" | nc -w $TIMEOUT $SERVER_CONTAINER $PORT
+  echo \"$MESSAGE\" | nc $SERVER_CONTAINER $PORT
 ")
 
 # Mostrar respuesta obtenida para debug
